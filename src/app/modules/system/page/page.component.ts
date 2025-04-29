d623e4cdd41bfc2d2355a7976e20b2753896ba22
KaioQuaresmadSouza <kaioquaresmadesouza1@gmail.com>
2025-04-29 16:36:48 -0300

    feat: Adicionado botão e parte de apresentação

--- src/app/modules/system/page/page.component.ts
+++ src/app/modules/system/page/page.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { HeroComponent } from '../hero/hero.component';
 
 @Component({
   selector: 'app-page',
-  imports: [HeaderComponent],
+  imports: [
+    HeaderComponent,
+    HeroComponent
+  ],
   templateUrl: './page.component.html',
   styleUrl: './page.component.scss'
 })
